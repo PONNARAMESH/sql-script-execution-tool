537eaecb56d1de3b92d5d8c301ac6f738f33c001
PONNARAMESH <ponnaramesh2016@gmail.com>
2025-03-14 15:47:06 +0530

    feat: setting up base project structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -6,11 +5,11 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Hi!! Buddy</h1>
-        <h3><i>Get start coding to build your UI for SQL-SCRIPT-EXECUTION-TOOL</i></h3>
+        <h3>
+          <i>Get start coding to build your UI for SQL-SCRIPT-EXECUTION-TOOL</i>
+        </h3>
       </main>
-      <footer className={styles.footer}>
-        Hey!! It's me from FOOTER
-      </footer>
+      <footer className={styles.footer}>Hey!! It&apos;s me from FOOTER</footer>
     </div>
   );
 }
